16222249a49d28b7ad169e0d79d47349224077be
Damian Barczyk <damian@barczyk.info>
2019-01-11 02:00:00 +0000

    Calc  form

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\CalcType;
 
-class DefaultController extends Controller
+class DefaultController extends AbstractController
 
 {
     public function index()
     {
-        return $this->render('default/index.html.twig', ['greeting' => 'hello']);
+        $form = $this->createForm(CalcType::class, array());
+        return $this->render('default/index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
     
     public function test() {
